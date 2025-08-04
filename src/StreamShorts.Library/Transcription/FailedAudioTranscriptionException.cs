1b2e6870349372d228f61f72e12327a2284ea980
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2025-08-03 22:15:07 -0500

    docs: add missing xml comments

--- src/StreamShorts.Library/Transcription/FailedAudioTranscriptionException.cs
+++ src/StreamShorts.Library/Transcription/FailedAudioTranscriptionException.cs
@@ -1,15 +1,30 @@
 namespace StreamShorts.Library.Transcription;
 
+/// <summary>
+/// Represents an error that occurs when audio transcription fails.
+/// </summary>
 public sealed class FailedAudioTranscriptionException : Exception
 {
+  /// <summary>
+  /// Initializes a new instance of the <see cref="FailedAudioTranscriptionException"/> class with no parameters.
+  /// </summary>
   public FailedAudioTranscriptionException()
   {
   }
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="FailedAudioTranscriptionException"/> class with a specified error message.
+  /// </summary>
+  /// <param name="message">The error message that explains the reason for the exception.</param>
   public FailedAudioTranscriptionException(string message) : base(message)
   {
   }
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="FailedAudioTranscriptionException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+  /// </summary>
+  /// <param name="message">The error message that explains the reason for the exception.</param>
+  /// <param name="innerException">The exception that is the cause of the current exception.</param>
   public FailedAudioTranscriptionException(string message, Exception innerException) : base(message, innerException)
   {
   }
